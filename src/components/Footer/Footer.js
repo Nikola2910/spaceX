bc0f2561104bb4e5ac15b464bb34a8a58c5ee959
Nikola2910 <nikolaivanovic2910@gmail.com>
2020-05-20 22:23:37 +0200

    added name to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.scss";
 
 function Footer() {
-  return <footer>SpaceX &copy;, March 2020</footer>;
+  return <footer>SpaceX &copy;, March 2020 Nikola</footer>;
 }
 
 export { Footer };
